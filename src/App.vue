d7cb7383bcbafe6f7b197390850ef6f03662c036
Alison Huang <alisonhcw@gmail.com>
2021-05-18 13:37:06 +0800

    install vue-router and add router setting

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,12 @@
 <template>
   <div id="app">
-    <div>
-      <div ref="selectType" class="current-type">{{ currentType }}</div>
-      <div class="switch-button" :class="{'stickyNav' : scrollToNav}" @click="selectType($event)">
-        <div v-for="(type, index) in types" :key="index" class="button">
-          <div>{{ type }}</div>
-        </div>
-      </div>
-    </div>
-    <TopTen />
-    <TotalNumber :currentType="currentType" />
-    <AllPet :currentType="currentType" />
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import TopTen from './components/TopTen.vue'
-import TotalNumber from '@/components/TotalNumber.vue'
-import AllPet from '@/components/AllPet.vue'
-
 export default {
-  name: 'App',
-  components: {
-    TopTen,
-    TotalNumber,
-    AllPet
-  },
-  data() {
-    return {
-      currentType: 'DOG',
-      types: ['DOG', 'CAT', 'OTHER', 'BIRD'],
-      scrollToNav: false
-    }
-  },
-  mounted() {
-    window.addEventListener('scroll', () => {
-      const height = this.$refs.selectType.offsetHeight
-      const {scrollTop} = document.documentElement
-      if (scrollTop > height) {
-        this.scrollToNav = true
-      } else {
-        this.scrollToNav = false
-      }
-    })
-  },
-  methods: {
-    selectType(e) {
-      this.currentType = e.target.innerText
-      console.log(this.currentType)
-    },
-    scrollDown() {
-      console.dir(this.$refs.selectType)
-    }
-  }
+  name: 'App'
 }
 </script>
 
@@ -62,32 +16,5 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-}
-.current-type {
-  padding: 20px 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-.switch-button {
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-}
-.stickyNav {
-  position: fixed;
-  top: 0;
-}
-.button {
-  width: 200px;
-  background-color: #fff;
-  border: 1px solid #ccc;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-.button:hover {
-  cursor: pointer;
 }
 </style>
